1a930ac8ff33e681a5e2f3a78fa12929f90ae058
Casey Wang <caseywang@httpeace.com>
2018-07-05 22:09:44 +0800

    config login saga

--- src/Redux/Auth/sagas.js
+++ src/Redux/Auth/sagas.js
@@ -1,1 +1,8 @@
-export const authSaga = function*() {};
+import { takeEvery } from "redux-saga/effects";
+import { actions } from "./actions";
+
+export const login = function*() {};
+
+export const authSaga = function*() {
+  yield takeEvery(actions.login, login);
+};
