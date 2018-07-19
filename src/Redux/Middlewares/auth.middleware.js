de65782d0f0256b49a4b32ecd9836e55095592c0
Casey Wang <caseywang@httpeace.com>
2018-07-19 12:18:15 +0800

    remove commend in auth.middleware

--- src/Redux/Middlewares/auth.middleware.js
+++ src/Redux/Middlewares/auth.middleware.js
@@ -3,18 +3,14 @@ export const authMiddleware = store => next => action => {
   const { meta } = action;
   let result = null;
   if (meta && meta.auth) {
-    // this action must authed, so we check store token
     const state = store.getState();
     if (state.auth.token) {
-      // pass
-      // maybe we should working with navigation ,now just pass do nothing
       result = next(action);
     } else {
-      //// maybe we should working with navigation ,now just pass do nothing
       result = NavigationServices.navigate("Login");
     }
   } else {
-    result = next(action); // other actions
+    result = next(action);
   }
   return result;
 };
