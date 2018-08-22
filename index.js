beb36cc33f02c40266d1c52923939791e6aa95b1
Casey Wang <caseywang@httpeace.com>
2018-08-22 15:30:56 +0800

    config yellowbox avoid isMounted is deprecated warning

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { App } from "./src/App";
+import { YellowBox } from "react-native";
 import { KeepAwake, registerRootComponent } from "expo";
 
+YellowBox.ignoreWarnings([
+  "Warning: isMounted(...) is deprecated",
+  "Module RCTImageLoader"
+]);
 if (__DEV__) {
   KeepAwake.activate();
 }
