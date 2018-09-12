09978ceae0d6e5d839f2587edd57a506569739ba
Casey Wang <caseywang@httpeace.com>
2018-09-13 01:12:26 +0800

    create send message mutation

--- src/Screens/Message/Containers/GraphqlRoom.js
+++ src/Screens/Message/Containers/GraphqlRoom.js
@@ -1,22 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { View } from "react-native";
 import ggl from "graphql-tag";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import { Toolbar } from "../../../Components/Toolbars/Toolbar";
 import { ToolbarContent } from "../../../Components/Toolbars/ToolbarContent";
 import { ToolbarBackAction } from "../../../Components/Toolbars/ToolbarBackAction";
-import { Chat, ThemeProvider } from "../../../Components/Chat";
+import {
+  Chat,
+  ThemeProvider,
+  InputToolBar,
+  Send
+} from "../../../Components/Chat";
 
 class GraphqlRoom extends Component {
   constructor(props) {
     super(props);
-    this._query = ggl`
+    this._QUERY_MESSAGES = ggl`
         query Room($room:String!){
             messages(roomId: $room){
                 id
                 text
             }
+        }
+    `;
+    this._CREATE_MESSAGE = ggl`
+        mutation CreateMessage($roomId:String!,$text:String!,$type:Int!){
+          createMessage(roomId:$roomId,text:$text,type:$type){
+            id
+            text
+          }
         }
     `;
   }
@@ -35,11 +49,37 @@ class GraphqlRoom extends Component {
       </Toolbar>
     );
   };
+  _renderActions = () => {
+    return (
+      <Mutation mutation={this._CREATE_MESSAGE}>
+        {createMessage => (
+          <View style={{ flex: 1, flexDirection: "row" }}>
+            <View style={{ flex: 1 }}>
+              <InputToolBar />
+            </View>
+            <View>
+              <Send
+                onPress={() => {
+                  createMessage({
+                    variables: {
+                      roomId: "5b76d92b65c7305de53818c1",
+                      text: "text",
+                      type: 1
+                    }
+                  });
+                }}
+              />
+            </View>
+          </View>
+        )}
+      </Mutation>
+    );
+  };
   render() {
     const { currentUser } = this.props;
     return (
       <Query
-        query={this._query}
+        query={this._QUERY_MESSAGES}
         variables={{
           room: "5b76d92b65c7305de53818c1"
         }}
@@ -54,6 +94,7 @@ class GraphqlRoom extends Component {
                 messages={data.messages}
                 user={currentUser}
                 renderMessage={props => <Chat.Message {...props} />}
+                renderActions={this._renderActions}
               />
             </ThemeProvider>
           );
